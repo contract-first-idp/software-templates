9904ebfc458340b2a018b02cd26c234b427e9836
David Gordon <davgordo@redhat.com>
2023-11-10 13:48:41 -0500

    dynamic api producer template

---
+++ templates/spring-boot-api-producer/skeleton/src/main/java/com/redhat/employee/Application.java
@@ -0,0 +1,30 @@
+package com.redhat.employee;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
+
+@SpringBootApplication(
+    nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
+@ComponentScan(
+    basePackages = {"com.redhat.employee", "com.redhat.employee.api" , "com.redhat.employee.config"},
+    nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class
+)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(name = "com.redhat.employee.Application.jsonNullableModule")
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+
+}
